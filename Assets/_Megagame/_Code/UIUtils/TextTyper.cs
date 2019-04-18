221bb6d4ae7c4f8d1e3a39875491203094d4b156
Graham Pentheny <graham.pentheny@gmail.com>
2019-04-18 03:22:08 -0400

    Text typer

---
+++ Assets/_Megagame/_Code/UIUtils/TextTyper.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextTyper : MonoBehaviour {
+	public float totalRevealTime = 10;
+
+	public void SetString(string value) {
+		GetComponent<TextMeshProUGUI>().text = "";
+		goalText = value;
+		var chars = goalText.ToCharArray();
+		charsToShow = new Queue<char>(chars);
+		secondsBetweenChars = totalRevealTime / chars.Length;
+		nextCharTime = Time.unscaledTime;
+	}
+
+	string goalText = "";
+	Queue<char> charsToShow = new Queue<char>();
+	float secondsBetweenChars = 0.001f;
+	float nextCharTime = 0;
+
+	void Start() {
+		GetComponent<TextMeshProUGUI>().text = "";
+	}
+
+	void Update() {
+		var tmp = GetComponent<TextMeshProUGUI>();
+		while (charsToShow.Count > 0 && Time.unscaledTime >= nextCharTime) {
+			nextCharTime += secondsBetweenChars;
+
+			// Add a char
+			var charToAdd = charsToShow.Dequeue();
+			tmp.text += charToAdd;
+
+			// skip over rich text tags
+			if (charToAdd == '<') {
+				while (charToAdd != '>') {
+					charToAdd = charsToShow.Dequeue();
+					tmp.text += charToAdd;
+				}
+			}
+		}
+	}
+}
