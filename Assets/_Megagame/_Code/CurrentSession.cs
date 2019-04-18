6de8fa5641476013391d93396b499427abcccc05
Graham Pentheny <graham.pentheny@gmail.com>
2019-04-18 03:22:45 -0400

    Session management

---
+++ Assets/_Megagame/_Code/CurrentSession.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CurrentSession : MonoBehaviour {
+	public static CurrentSession Instance;
+
+	public string AccessKey;
+
+	void Awake() {
+		Instance = this;
+	}
+
+	public void ClearCurrentSession() {
+		AccessKey = null;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (string.IsNullOrEmpty(AccessKey)) {
+				Application.Quit();
+			} else {
+				ClearCurrentSession();
+				SceneManager.LoadScene("Login", LoadSceneMode.Single);
+			}
+		}
+	}
+}
